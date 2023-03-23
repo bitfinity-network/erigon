3ff40f08509cfc6d63f334694b31a8984c260655
Dmytro Gordon <g-d@ukr.net>
2023-03-23 17:23:12 +0200

    Fix importing transactions for new custom token creation

--- cmd/blockimporter/state.go
+++ cmd/blockimporter/state.go
@@ -29,6 +29,9 @@ type State struct {
 
 // Account that is used to perform withdraws/deposits
 const ownerAccount string = "0x0000000000000000000000000000000000000000"
+
+//TODO: uncomment this when ic agent registration is implemented is implemented
+//const ownerAccount string = "0xb0e5863d0ddf7e105e409fee0ecc0123a362e14b"
 const chainID int64 = 355113
 
 func NewState(db *DB) (*State, error) {
@@ -53,6 +56,23 @@ func NewState(db *DB) (*State, error) {
 		genesis.Alloc = initState
 		chainConfig := chain.Config{
 			ChainID: big.NewInt(chainID),
+
+			// Set the chain spec corresponding to the latest one from revm
+			HomesteadBlock:        big.NewInt(0),
+			DAOForkBlock:          big.NewInt(0),
+			DAOForkSupport:        true,
+			TangerineWhistleBlock: big.NewInt(0),
+			SpuriousDragonBlock:   big.NewInt(0),
+			ByzantiumBlock:        big.NewInt(0),
+			ConstantinopleBlock:   big.NewInt(0),
+			PetersburgBlock:       big.NewInt(0),
+			IstanbulBlock:         big.NewInt(0),
+			MuirGlacierBlock:      big.NewInt(0),
+			BerlinBlock:           big.NewInt(0),
+			LondonBlock:           big.NewInt(0),
+			ArrowGlacierBlock:     big.NewInt(0),
+			GrayGlacierBlock:      big.NewInt(0),
+			MergeNetsplitBlock:    big.NewInt(0),
 		}
 		genesis.Config = &chainConfig
 		_, _, err := core.CommitGenesisBlock(db.GetChain(), &genesis)
@@ -91,7 +111,7 @@ func (state *State) ProcessBlock(block types.Block) error {
 	// TODO: remove it when all the transactions are signed
 	// https: //infinityswap.atlassian.net/browse/EPROD-203
 	for _, tr := range block.Transactions() {
-		if legacyTr, ok := tr.(*types.LegacyTx); ok {
+		if legacyTr, ok := tr.(*types.LegacyTx); ok && legacyTr.V.IsZero() {
 			legacyTr.V = *uint256.NewInt(27)
 		}
 	}
